e950f9aa508264f97035397822b18898f21ac3e7
hien nguyen <thuhiennguyen198@gmail.com>
2021-06-23 23:00:30 -0400

    Finish

--- battleship_project/lib/battleship.rb
+++ battleship_project/lib/battleship.rb
@@ -3,6 +3,55 @@ require_relative "player"
 
 class Battleship
 
-   
+    attr_reader :board, :player
+   def initialize(n)
+    @player = Player.new
+    @board = Board.new(n)
+    @remaining_misses = @board.size / 2
+   end
+
+    def start_game
+        @board.place_random_ships
+        puts "#{@board.num_ships}"
+        @board.print
+    end
+
+    def lose?
+        if @remaining_misses <= 0
+            puts "you lose"
+            return true
+        else
+            return false
+        end
+    end
+
+    def win?
+        if @board.num_ships == 0
+        puts "you win"
+        return true
+        else
+            return false
+        end
+    end
+
+
+    def game_over?
+        if self.win? || self.lose?
+            return true
+        else
+            return false
+        end
+    end
+
+    def turn
+        
+        pos = @player.get_move
+        if !board.attack(pos)
+           @remaining_misses -= 1
+        end 
+        @board.print
+        puts "The remaining_misses is: #{@remaining_misses}"
+    end
 
 end
+
